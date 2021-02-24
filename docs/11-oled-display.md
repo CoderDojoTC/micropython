172e350e73bd5a15a23b21d2092f828b89d64769
dmccreary <dan.mccreary@gmail.com>
2021-02-23 20:22:05 -0600

    updated content

--- docs/11-oled-display.md
+++ docs/11-oled-display.md
@@ -141,18 +141,56 @@ print('done')
 ## SSD1306 vs. SH1106
 There is only one small difference between SSD1306 and SH1106: The SH1106 controller has an internal RAM of 132x64 pixel. The SSD1306 only has 128x64 pixel.
 
-## Using the 7-wire SPI interface
+## The SPI interface
 The four wire I2C interface is great for kids that don't want to hook up more than four wires.  But there are times when we want a higher performance screen with faster refresh times.  This is when the SPI interface comes in handy.
 
 The seven wires on the back of the SPI OLED screens are the following as read from the top to bottom looking at the back of the display:
 
+![](img/oled-back-connections.png)
+
 1. CS - Chip Select - pin 4
 2. DC - Data/Command - pin 5
 3. RES - Reset - pin 6
-4. SDA - Data - pin 1
-5. SCL - Clock - pin 2
+4. SDA - Data - SPIO TX GP7 pin 10
+5. SCL - Clock - Connect to SPIO SCK GP6 pin 9
 6. VCC - Connect to the 3.3V Out pin 36
 7. GND - pin 38 or 3 any other GND pin
+
+
+### Pico Pins
+
+```
+# Sample code sections
+ 28 # ------------ SPI ------------------
+ 29 # Pin Map SPI
+ 30 # - 3v - xxxxxx - Vcc
+ 31 # - G - xxxxxx - Gnd
+ 32 # - D7 - GPIO 13 - Din / MOSI fixed
+ 33 # - D5 - GPIO 14 - Clk / Sck fixed
+ 34 # - D8 - GPIO 4 - CS (optional, if the only connected device)
+ 35 # - D2 - GPIO 5 - D/C
+ 36 # - D1 - GPIO 2 - Res
+```
+
+SCK is the clock - hook this to the oled SCL
+MOSI is the line taking data from your Pico to the peripheral device.  Hook this to SDA
+
+From the SDK:
+https://datasheets.raspberrypi.org/pico/raspberry-pi-pico-python-sdk.pdf
+Section 3.7
+
+1. SPI0_SCK - pin 6
+2. SPI0_MOSI - pin 7
+3. SPI0_MISO - pin 8
+
+This contradicts p122 in GET STARTED WITH MICROPYTHON ON RASPBERRY PI PICO
+
+```
+spi_sck=machine.Pin(2)
+spi_tx=machine.Pin(3)
+spi_rx=machine.Pin(4)
+```
+
 
 ### SPI Terms
 Master Out Slave In (MOSI)
@@ -170,25 +208,45 @@ From the documentation:
     **dc** is the GPIO Pin object for the Data/Command selection. It will be initialized by the driver.
 
     **res** is the GPIO Pin object for the reset connection. It will be initialized by the driver. If it is not needed, it can be set to None or omitted. In this case the default value of None applies.
-    
+
     **cs** is the GPIO Pin object for the CS connection. It will be initialized by the driver. If it is not needed, it can be set to None or omitted. In this case the default value of None applies.
 
 ```py
 import machine
+import machine
+import utime
 import ssd1306
-spi = machine.SPI(0, baudrate=400000, sck=Pin(2), mosi=Pin(1))
+led = machine.Pin(25, machine.Pin.OUT)
 
-# pin 0 - SCL
-# pin 1 - SDA
 # From: https://github.com/robert-hh/SH1106
 # display = sh1106.SH1106_SPI(width, height, spi, dc, res, cs)
-DC = machine.Pin(5)
-RES = machine.Pin(6)
-CS = machine.Pin(4)
+#MOSI=machine.Pin(7)
+#SCK=machine.Pin(6)
+#spi = machine.SPI(0, baudrate=400000, sck=SCK, mosi=MOSI)
+spi_sck=machine.Pin(6)
+spi_tx=machine.Pin(7)
+# spi_rx=machine.Pin(4)
+spi=machine.SPI(0,baudrate=100000,sck=spi_sck, mosi=spi_tx)
+
+CS = machine.Pin(8)
+DC = machine.Pin(9)
+RES = machine.Pin(10)
+
 oled = ssd1306.SSD1306_SPI(128, 64, spi, DC, RES, CS)
+
+# flash all pixels on
+oled.fill(1)
+oled.show()
+utime.sleep(0.5)
+
 oled.fill(0)
 oled.text('CoderDojo Rocks!', 0, 0, 1)
 oled.show()
+
+# flash the LED to show end
+led.high()
+utime.sleep(0.5)
+led.low()
 
 print('Done')
 ```
@@ -202,3 +260,5 @@ https://www.mfitzp.com/article/oled-displays-i2c-micropython/
 https://github.com/adafruit/Adafruit_CircuitPython_SSD1306/blob/master/examples/ssd1306_stats.py
 
 https://github.com/robert-hh/SH1106/blob/master/sh1106.py
+
+[DIY More OLED Product Description](https://www.diymore.cc/collections/all-about-arduino/products/2-42-inch-12864-oled-display-module-iic-i2c-spi-serial-for-arduino-c51-stm32-green-white-blue-yellow?variant=17060396597306)
---
+++ src/oled/oled-spi-working.py
@@ -0,0 +1,150 @@
+# SCK  -> GP6
+# MOSI -> GP7
+# CS   -> GP5
+# D/C  -> GP9
+# RES  -> GP8
+
+from machine import Pin, SPI
+from micropython import const
+import utime as time
+import framebuf
+
+
+# a few register definitions
+_SET_CONTRAST        = const(0x81)
+_SET_NORM_INV        = const(0xa6)
+_SET_DISP            = const(0xae)
+_SET_SCAN_DIR        = const(0xc0)
+_SET_SEG_REMAP       = const(0xa0)
+_LOW_COLUMN_ADDRESS  = const(0x00)
+_HIGH_COLUMN_ADDRESS = const(0x10)
+_SET_PAGE_ADDRESS    = const(0xB0)
+
+
+class SH1106:
+    def __init__(self, width, height, external_vcc):
+        self.width = width
+        self.height = height
+        self.external_vcc = external_vcc
+        self.pages = self.height // 8
+        self.buffer = bytearray(self.pages * self.width)
+        fb = framebuf.FrameBuffer(self.buffer, self.width, self.height,
+                                  framebuf.MVLSB)
+        self.framebuf = fb
+# set shortcuts for the methods of framebuf
+        self.fill = fb.fill
+        self.fill_rect = fb.fill_rect
+        self.hline = fb.hline
+        self.vline = fb.vline
+        self.line = fb.line
+        self.rect = fb.rect
+        self.pixel = fb.pixel
+        self.scroll = fb.scroll
+        self.text = fb.text
+        self.blit = fb.blit
+
+        self.init_display()
+
+    def init_display(self):
+        self.reset()
+        self.fill(0)
+        self.poweron()
+        self.show()
+
+    def poweroff(self):
+        self.write_cmd(_SET_DISP | 0x00)
+
+    def poweron(self):
+        self.write_cmd(_SET_DISP | 0x01)
+
+    def rotate(self, flag, update=True):
+        if flag:
+            self.write_cmd(_SET_SEG_REMAP | 0x01)  # mirror display vertically
+            self.write_cmd(_SET_SCAN_DIR | 0x08)  # mirror display hor.
+        else:
+            self.write_cmd(_SET_SEG_REMAP | 0x00)
+            self.write_cmd(_SET_SCAN_DIR | 0x00)
+        if update:
+            self.show()
+
+    def sleep(self, value):
+        self.write_cmd(_SET_DISP | (not value))
+
+    def contrast(self, contrast):
+        self.write_cmd(_SET_CONTRAST)
+        self.write_cmd(contrast)
+
+    def invert(self, invert):
+        self.write_cmd(_SET_NORM_INV | (invert & 1))
+
+    def show(self):
+        for page in range(self.height // 8):
+            self.write_cmd(_SET_PAGE_ADDRESS | page)
+            self.write_cmd(_LOW_COLUMN_ADDRESS | 2)
+            self.write_cmd(_HIGH_COLUMN_ADDRESS | 0)
+            self.write_data(self.buffer[
+                self.width * page:self.width * page + self.width
+            ])
+
+    def reset(self, res):
+        if res is not None:
+            res(1)
+            time.sleep_ms(1)
+            res(0)
+            time.sleep_ms(20)
+            res(1)
+            time.sleep_ms(20)
+
+
+
+class SH1106_SPI(SH1106):
+    def __init__(self, width, height, spi, dc, res=None, cs=None,
+                 external_vcc=False):
+
+        self.spi = spi
+        self.dc = dc
+        self.res = res
+        self.cs = cs
+        self.spi.init()
+        dc.init(dc.OUT, value=0)
+        if res is not None:
+            res.init(res.OUT, value=0)
+        if cs is not None:
+            cs.init(cs.OUT, value=0)
+
+        super().__init__(width, height, external_vcc)
+
+    def write_cmd(self, cmd):
+        #self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        if self.cs is not None:
+            #self.cs(1)
+            self.dc(0)
+            self.cs(0)
+            self.spi.write(bytearray([cmd]))
+            self.cs(1)
+        else:
+            self.dc(0)
+            self.spi.write(bytearray([cmd]))
+
+    def write_data(self, buf):
+        #self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        if self.cs is not None:
+            #self.cs(1)
+            self.dc(1)
+            self.cs(0)
+            self.spi.write(buf)
+            self.cs(1)
+        else:
+            self.dc(1)
+            self.spi.write(buf)
+
+    def reset(self):
+        super().reset(self.res)
+        
+spi = SPI(0, baudrate=1000*1000, mosi=Pin(7))
+display = SH1106_SPI(96, 64, spi, Pin(9), Pin(8), Pin(5))
+display.sleep(False)
+display.fill(0)
+display.text('Testing 1', 20, 20, 1)
+#display.poweroff()
+print(spi)
