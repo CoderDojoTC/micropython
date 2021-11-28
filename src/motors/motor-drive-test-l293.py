aa6575a62c9df649e8979cca72cdf8585375b83c
dmccreary <dan.mccreary@gmail.com>
2021-11-28 16:52:36 -0600

    updated content

--- src/motors/motor-drive-test-l293.py
+++ src/motors/motor-drive-test-l293.py
@@ -21,15 +21,33 @@ def spin_wheel(pwm):
     pwm.duty_u16(0)
     sleep(1)
 
-while True:
-    print('right forward')
-    spin_wheel(right_forward)
+def stop():
+    right_forward.duty_u16(0)
+    right_reverse.duty_u16(0)
+    left_forward.duty_u16(0)
+    left_reverse.duty_u16(0)    
     
-    print('right reverse')
-    spin_wheel(right_reverse)
-    
-    print('left foward')
-    spin_wheel(left_forward)
-    
-    print('left_reverse')
-    spin_wheel(left_reverse)
+def main():
+    while True:
+        print('right forward')
+        spin_wheel(right_forward)
+        
+        print('right reverse')
+        spin_wheel(right_reverse)
+        
+        print('left foward')
+        spin_wheel(left_forward)
+        
+        print('left_reverse')
+        spin_wheel(left_reverse)
+# end of main()
+
+try:
+    main()
+except KeyboardInterrupt:
+    print('Got ctrl-c')
+finally:
+    # Cleanup code
+    print('Cleaning up')
+    print('Powering down all motors now.')
+    stop()
