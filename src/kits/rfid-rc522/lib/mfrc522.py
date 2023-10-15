731e3919dc958adf72bffb325d255e5ef0da2579
Dan Mccreary <Dan.McCreary@gmail.com>
2023-10-15 17:31:20 -0500

    updating content

--- src/kits/rfid-rc522/lib/mfrc522.py
+++ src/kits/rfid-rc522/lib/mfrc522.py
@@ -1,14 +1,16 @@
+#mfrc522.py
+
 from machine import Pin, SPI
 from os import uname
-
-
+ 
+ 
 class MFRC522:
-
+ 
     DEBUG = False
     OK = 0
     NOTAGERR = 1
     ERR = 2
-
+ 
     REQIDL = 0x26
     REQALL = 0x52
     AUTHENT1A = 0x60
@@ -18,21 +20,20 @@ class MFRC522:
     PICC_ANTICOLL2 = 0x95
     PICC_ANTICOLL3 = 0x97
   
-
+ 
     def __init__(self, sck, mosi, miso, rst, cs,baudrate=1000000,spi_id=0):
-
+ 
         self.sck = Pin(sck, Pin.OUT)
-        self.mosi = Pin(3, Pin.OUT)
-        # made it an input pin
-        self.miso = Pin(miso, Pin.IN)
+        self.mosi = Pin(mosi, Pin.OUT)
+        self.miso = Pin(miso)
         self.rst = Pin(rst, Pin.OUT)
         self.cs = Pin(cs, Pin.OUT)
-
+ 
         self.rst.value(0)
         self.cs.value(1)
         
         board = uname()[0]
-
+ 
         if board == 'WiPy' or board == 'LoPy' or board == 'FiPy':
             self.spi = SPI(0)
             self.spi.init(SPI.MASTER, baudrate=1000000, pins=(self.sck, self.mosi, self.miso))
@@ -40,75 +41,73 @@ class MFRC522:
             self.spi = SPI(baudrate=100000, polarity=0, phase=0, sck=self.sck, mosi=self.mosi, miso=self.miso)
             self.spi.init()
         elif board == 'rp2':
-            print('found rp2')
-            
-            self.spi = SPI(spi_id, baudrate=baudrate, sck=self.sck,  mosi=self.mosi,  miso=self.miso)
+            self.spi = SPI(spi_id,baudrate=baudrate,sck=self.sck, mosi= self.mosi, miso= self.miso)
         else:
             raise RuntimeError("Unsupported platform")
-
+ 
         self.rst.value(1)
         self.init()
-
+ 
     def _wreg(self, reg, val):
-
+ 
         self.cs.value(0)
         self.spi.write(b'%c' % int(0xff & ((reg << 1) & 0x7e)))
         self.spi.write(b'%c' % int(0xff & val))
         self.cs.value(1)
-
+ 
     def _rreg(self, reg):
-
+ 
         self.cs.value(0)
         self.spi.write(b'%c' % int(0xff & (((reg << 1) & 0x7e) | 0x80)))
         val = self.spi.read(1)
         self.cs.value(1)
-
+ 
         return val[0]
-
+ 
     def _sflags(self, reg, mask):
         self._wreg(reg, self._rreg(reg) | mask)
-
+ 
     def _cflags(self, reg, mask):
         self._wreg(reg, self._rreg(reg) & (~mask))
-
+ 
     def _tocard(self, cmd, send):
-
+ 
         recv = []
         bits = irq_en = wait_irq = n = 0
         stat = self.ERR
-
+ 
         if cmd == 0x0E:
             irq_en = 0x12
             wait_irq = 0x10
         elif cmd == 0x0C:
             irq_en = 0x77
             wait_irq = 0x30
-
+ 
         self._wreg(0x02, irq_en | 0x80)
         self._cflags(0x04, 0x80)
         self._sflags(0x0A, 0x80)
         self._wreg(0x01, 0x00)
-
+ 
         for c in send:
             self._wreg(0x09, c)
         self._wreg(0x01, cmd)
-
+ 
         if cmd == 0x0C:
             self._sflags(0x0D, 0x80)
-
+ 
         i = 2000
         while True:
             n = self._rreg(0x04)
             i -= 1
             if ~((i != 0) and ~(n & 0x01) and ~(n & wait_irq)):
                 break
-
+ 
         self._cflags(0x0D, 0x80)
-
+ 
         if i:
             if (self._rreg(0x06) & 0x1B) == 0x00:
                 stat = self.OK
-
+ 
                 if n & irq_en & 0x01:
                     stat = self.NOTAGERR
                 elif cmd == 0x0C:
@@ -118,40 +117,40 @@ class MFRC522:
                         bits = (n - 1) * 8 + lbits
                     else:
                         bits = n * 8
-
+ 
                     if n == 0:
                         n = 1
                     elif n > 16:
                         n = 16
-
+ 
                     for _ in range(n):
                         recv.append(self._rreg(0x09))
             else:
                 stat = self.ERR
-
+ 
         return stat, recv, bits
-
+ 
     def _crc(self, data):
-
+ 
         self._cflags(0x05, 0x04)
         self._sflags(0x0A, 0x80)
-
+ 
         for c in data:
             self._wreg(0x09, c)
-
+ 
         self._wreg(0x01, 0x03)
-
+ 
         i = 0xFF
         while True:
             n = self._rreg(0x05)
             i -= 1
             if not ((i != 0) and not (n & 0x04)):
                 break
-
+ 
         return [self._rreg(0x22), self._rreg(0x21)]
-
+ 
     def init(self):
-
+ 
         self.reset()
         self._wreg(0x2A, 0x8D)
         self._wreg(0x2B, 0x3E)
@@ -160,35 +159,35 @@ class MFRC522:
         self._wreg(0x15, 0x40)
         self._wreg(0x11, 0x3D)
         self.antenna_on()
-
+ 
     def reset(self):
         self._wreg(0x01, 0x0F)
-
+ 
     def antenna_on(self, on=True):
-
+ 
         if on and ~(self._rreg(0x14) & 0x03):
             self._sflags(0x14, 0x03)
         else:
             self._cflags(0x14, 0x03)
-
+ 
     def request(self, mode):
-
+ 
         self._wreg(0x0D, 0x07)
         (stat, recv, bits) = self._tocard(0x0C, [mode])
-
+ 
         if (stat != self.OK) | (bits != 0x10):
             stat = self.ERR
-
+ 
         return stat, bits
   
     def anticoll(self,anticolN):
-
+ 
         ser_chk = 0
         ser = [anticolN, 0x20]
-
+ 
         self._wreg(0x0D, 0x00)
         (stat, recv, bits) = self._tocard(0x0C, ser)
-
+ 
         if stat == self.OK:
             if len(recv) == 5:
                 for i in range(4):
@@ -197,9 +196,9 @@ class MFRC522:
                     stat = self.ERR
             else:
                 stat = self.ERR
-
+ 
         return stat, recv
-
+ 
     
     def PcdSelect(self, serNum,anticolN):
         backData = []
@@ -282,7 +281,7 @@ class MFRC522:
                 if status != self.OK:
                     return (self.ERR,[])
                 if self.DEBUG: print("Anticol(3) {}".format(uid))
-                if self.PcdSelect(uid,self.PICC_ANTICOLL3) == 0:
+                if self.MFRC522_PcdSelect(uid,self.PICC_ANTICOLL3) == 0:
                     return (self.ERR,[])
                 if self.DEBUG: print("PcdSelect(3) {}".format(uid))
         valid_uid.extend(uid[0:5])
@@ -296,7 +295,7 @@ class MFRC522:
    
        
     
-
+ 
     def auth(self, mode, addr, sect, ser):
         return self._tocard(0x0E, [mode, addr] + sect + ser[:4])[0]
     
@@ -308,23 +307,23 @@ class MFRC522:
             status = self.auth(self.AUTHENT1B, addr, keyB, uid)
         return status
        
-
+ 
     def stop_crypto1(self):
         self._cflags(0x08, 0x08)
-
+ 
     def read(self, addr):
-
+ 
         data = [0x30, addr]
         data += self._crc(data)
         (stat, recv, _) = self._tocard(0x0C, data)
         return stat, recv
-
+ 
     def write(self, addr, data):
-
+ 
         buf = [0xA0, addr]
         buf += self._crc(buf)
         (stat, recv, bits) = self._tocard(0x0C, buf)
-
+ 
         if not (stat == self.OK) or not (bits == 4) or not ((recv[0] & 0x0F) == 0x0A):
             stat = self.ERR
         else:
@@ -336,8 +335,8 @@ class MFRC522:
             if not (stat == self.OK) or not (bits == 4) or not ((recv[0] & 0x0F) == 0x0A):
                 stat = self.ERR
         return stat
-
-
+ 
+ 
     def writeSectorBlock(self,uid, sector, block, data, keyA=None, keyB = None):
         absoluteBlock =  sector * 4 + (block % 4)
         if absoluteBlock > 63 :
@@ -347,7 +346,7 @@ class MFRC522:
         if self.authKeys(uid,absoluteBlock,keyA,keyB) != self.ERR :
             return self.write(absoluteBlock, data)
         return self.ERR
-
+ 
     def readSectorBlock(self,uid ,sector, block, keyA=None, keyB = None):
         absoluteBlock =  sector * 4 + (block % 4)
         if absoluteBlock > 63 :
@@ -355,7 +354,7 @@ class MFRC522:
         if self.authKeys(uid,absoluteBlock,keyA,keyB) != self.ERR :
             return self.read(absoluteBlock)
         return self.ERR, None
-
+ 
     def MFRC522_DumpClassic1K(self,uid, Start=0, End=64, keyA=None, keyB=None):
         for absoluteBlock in range(Start,End):
             status = self.authKeys(uid,absoluteBlock,keyA,keyB)
@@ -381,5 +380,3 @@ class MFRC522:
             print("Authentication error")
             return self.ERR
         return self.OK
-        
-                
